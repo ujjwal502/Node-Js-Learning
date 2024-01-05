1c421de8d59ab945f96dbe2fc84c01b52439a5e7
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2024-01-05 21:35:12 +0530

    initial commit

---
+++ express js/app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
